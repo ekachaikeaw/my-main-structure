c62e0fdb5e15ed9bc539d9337e4d9ac0bd059e5d
ekachaikeaw <146946429+ekachaikeaw@users.noreply.github.com>
2025-03-14 21:18:23 +0700

    add example

--- internal/http/rest/some_routes.go
+++ internal/http/rest/some_routes.go
@@ -1,2 +1,33 @@
 // create server register routes method here
 // use web framework Group instance and define routes with handler
+
+package server
+
+import (
+	_inventoryRepository "github.com/repo-name/isekai-shop-api/pkg/inventory/repository"
+	_itemShopController "github.com/repo-name/isekai-shop-api/pkg/itemShop/controller"
+	_itemShopRepository "github.com/repo-name/isekai-shop-api/pkg/itemShop/repository"
+	_itemShopService "github.com/repo-name/isekai-shop-api/pkg/itemShop/service"
+	_playerCoinRepository "github.com/repo-name/isekai-shop-api/pkg/playerCoin/repository"
+)
+
+func (s *echoServer) initItemShopRouter(m *authorizingMiddleware) {
+	router := s.app.Group("/v1/item-shop")
+
+	playerCoinRepository := _playerCoinRepository.NewPlayerCoinRepositoryImpl(s.db, s.app.Logger)
+	inventoryRepository := _inventoryRepository.NewInventoryRepositoryImpl(s.db, s.app.Logger)
+	itemShopRepository := _itemShopRepository.NewItemShopRepositoryImpl(s.db, s.app.Logger)
+
+	itemShopService := _itemShopService.NewItemShopServiceImpl(
+		itemShopRepository,
+		playerCoinRepository,
+		inventoryRepository,
+		s.app.Logger,
+	)
+
+	itemShopController := _itemShopController.NewItemShopControllerImpl(itemShopService)
+
+	router.GET("", itemShopController.Listing)
+	router.POST("/buying", itemShopController.Buying, m.PlayerAuthorizing)
+	router.POST("/selling", itemShopController.Selling, m.PlayerAuthorizing)
+}
